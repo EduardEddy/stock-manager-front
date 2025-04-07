48fc950618fdb5d829eae9595a2862b2b3601549
Eduard Acevedo <eduardeddyacevedo@gmail.com>
2025-04-07 09:02:36 -0500

    change api server

--- src/config/envs.js
+++ src/config/envs.js
@@ -1,7 +1,7 @@
-const apiServer = process.env.REACT_APP_API_SERVER;
+const apiServer = process.env.API_SERVER;
 
 if (!apiServer) {
-  throw new Error('REACT_APP_API_SERVER is not defined in .env');
+  throw new Error('API_SERVER is not defined in .env');
 }
 
 export const envs = {
